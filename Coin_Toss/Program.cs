72f65251896838e07a20fda024265edd4eb401a3
Italo Thiago <italothiago220@gmail.com>
2024-04-22 13:38:55 -0300

    feat: add Coin_Toss

---
+++ Coin_Toss/Program.cs
@@ -0,0 +1,8 @@
+﻿// My solution
+// Random random = new Random();
+// int Face = random.Next(1,10);
+// Console.WriteLine($"Coin is :{(Face >= 5 ? "heads" : "tails")}");
+
+// Course Solution
+Random coin = new Random();
+Console.WriteLine($"Coin is: {(coin.Next(0,2) == 0 ? "heads" : "tails")}");
