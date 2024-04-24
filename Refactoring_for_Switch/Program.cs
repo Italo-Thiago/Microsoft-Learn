1a1a483b2747ce619ef01b1a072734618fb331c3
Italo Thiago <italothiago220@gmail.com>
2024-04-24 13:01:03 -0300

    feat: Complete Refactoring Challenge

---
+++ Refactoring_for_Switch/Program.cs
@@ -0,0 +1,59 @@
+﻿// SKU = Stock Keeping Unit
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+// product
+switch (product[0])
+{
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat pants";
+        break;
+    default:
+        type = "Other";
+        break;
+}
+
+// color
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Marron";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+// size
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
