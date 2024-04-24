3b7ea8f0e681c69d13156cc9fc35c98e0f6c9d95
Italo Thiago <italothiago220@gmail.com>
2024-04-23 23:07:19 -0300

    feat: Simple_Refactoring

---
+++ Simple_Refactoring/Program.cs
@@ -0,0 +1,33 @@
+﻿// Microsoft Learn
+
+// int[] numbers = { 4, 8, 15, 16, 23, 42 };
+// int total = 0;
+// bool found = false;
+
+// foreach (int number in numbers)
+// {
+//     total += number;
+//     if (number == 42)
+//         found = true;
+// }
+
+// if (found)
+//     Console.WriteLine("Set contains 42");
+
+// Console.WriteLine($"Total: {total}");
+
+// My Solution
+int[] numbers = {4, 8, 15, 16, 23, 42};
+int total = 0;
+
+foreach (int number in numbers)
+{
+    total += number;
+    if (number == 42)
+    {
+        bool found = true;
+        if (found)
+            Console.WriteLine("Set contains 42");
+        Console.WriteLine($"Total: {total}");
+    }
+}
