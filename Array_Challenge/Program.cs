776d5db1cb0c5a42ec742933dfcbdbc08cc891db
Italo Thiago <italothiago220@gmail.com>
2024-05-03 11:57:16 -0300

    feat: add Array Challenger

---
+++ Array_Challenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+string[] values = { "12.3", "45", "ABC", "11", "DEF"};
+string message = "";
+string total;
+
+// Crie um loop que ira passar por todos os índices => foreach ou for
+
+
+// Se o valor for um número adicione a "total" caso contrario concatene com "message"
