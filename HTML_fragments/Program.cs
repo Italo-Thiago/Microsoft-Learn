0884509c3a7e05c23b8bd9fe5ab9fada86693656
Italo Thiago <italothiago220@gmail.com>
2024-05-14 17:58:27 -0300

    feat: add 14/05 files

---
+++ HTML_fragments/Program.cs
@@ -0,0 +1,61 @@
+﻿// My Solution
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+
+int initQuantity = input.IndexOf("5");
+int length = 4;
+
+quantity = input.Substring(initQuantity,length);
+
+int initOutput = input.IndexOf("h");
+int endOutput = input.LastIndexOf("n");
+
+initOutput -= 1;
+endOutput += 2;
+
+int lenghtOutput = endOutput - initOutput;
+output = input.Substring(initOutput, lenghtOutput);
+
+output = output.Replace("&trade;","&reg;");
+
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");
+
+// Microsoft Learn Solution
+
+quantity = "";
+output = "";
+
+// Your work here
+
+// Extract the "quantity"
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int quantityStart = input.IndexOf(openSpan) + openSpan.Length; // + length of <span> so index at end of <span> tag
+int quantityEnd= input.IndexOf(closeSpan);
+int quantityLength = quantityEnd - quantityStart;
+quantity = input.Substring(quantityStart, quantityLength);
+quantity = $"Quantity: {quantity}";
+
+// Set output to input, replacing the trademark symbol with the registered trademark symbol
+const string tradeSymbol = "&trade;";
+const string regSymbol = "&reg;";
+output = input.Replace(tradeSymbol, regSymbol);
+
+// Remove the opening <div> tag
+const string openDiv = "<div>";
+int divStart = output.IndexOf(openDiv);
+output = output.Remove(divStart, openDiv.Length);
+
+// Remove the closing </div> tag and add "Output:" to the beginning
+const string closeDiv = "</div>";
+int divCloseStart = output.IndexOf(closeDiv);
+output = "Output: " + output.Remove(divCloseStart, closeDiv.Length);
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
