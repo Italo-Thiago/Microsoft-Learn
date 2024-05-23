489dc16dd2d6f3181cda000f91118d238d270128
Italo Thiago <italothiago220@gmail.com>
2024-05-23 10:20:50 -0300

    23/05

---
+++ Methods/MethodsFive/Program.cs
@@ -0,0 +1,70 @@
+﻿// My Solution
+string[,] corporate = 
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "@hayworth.com";
+string corporateDomain = "@contoso.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+    emailFormat(i,corporate,corporateDomain);
+}
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+    emailFormat(i,external,externalDomain);
+}
+
+void emailFormat(int index, string[,] array, string email)
+{
+    string firstName = array[index,0].ToLower();
+    string firstTwoCharacter = firstName.Remove(2);
+
+    string secondName = array[index,1].ToLower();
+
+    Console.WriteLine(firstTwoCharacter + secondName + email);
+}
+
+// Microsoft Learn Solution
+/*
+string[,] corporate = 
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+    DisplayEmail(first: corporate[i,0], last: corporate[i,1]);
+}
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+    DisplayEmail(first: external[i,0], last: external[i,1], domain: externalDomain);
+}
+
+void DisplayEmail(string first, string last, string domain = "contoso.com") 
+{
+    string email = first.Substring(0, 2) + last;
+    email = email.ToLower();
+    Console.WriteLine($"{email}@{domain}");
+}
+*/
---
+++ Methods/MethodsSix/Program.cs
@@ -0,0 +1,33 @@
+﻿// Method Return
+// string input = "snake";
+
+// Console.WriteLine(input);
+// Console.WriteLine(ReverseWord(input));
+
+string input = "there are snakes at the zoo";
+
+Console.WriteLine(input);
+Console.WriteLine(ReverseSentence(input));
+
+string ReverseWord(string word) 
+{
+    string result = "";
+    for (int i = word.Length - 1; i >= 0; i--) 
+    {
+        result += word[i];
+    }
+    return result;
+}
+
+string ReverseSentence(string input) 
+{
+    string result = "";
+    string[] words = input.Split(" ");
+
+    foreach(string word in words) 
+    {
+        result += ReverseWord(word) + " ";
+    }
+
+    return result.Trim();
+}
