2310a3d3f6d616ff560064d9800c28bbf5cba242
Italo Thiago <italothiago220@gmail.com>
2024-05-28 09:17:12 -0300

    feat: add another try-catch example

--- Exceptions/ThrowOne/obj/Debug/net8.0/ThrowOne.AssemblyInfo.cs
+++ Exceptions/ThrowOne/obj/Debug/net8.0/ThrowOne.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThrowOne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61b9dfccdb697c2390e7147f02a24d556ac7c74b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dd0b6dde540ff9cd7f125bdf13cca2aae209254")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThrowOne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThrowOne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
